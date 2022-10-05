baf51a2b25e01da2db83f45b9c78065667d34f1c
Javi Latorre <jlatorre@ratedpower.com>
2022-10-05 14:28:32 +0200

    CRA frontend skeleton base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { AppContainer } from "./App.styled";
+import { Home } from "./pages/Home";
+
+const App = () => {
+  return (
+    <AppContainer>
+      <Home />
+    </AppContainer>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/NavBar/Navbar.styled.ts
@@ -0,0 +1,16 @@
+import Styled from "styled-components";
+
+export const NavBarContainer = Styled.div`
+  background: #154064;
+  height: 60px;
+  display: flex;
+  align-items: center;
+
+  p {
+    color: white;
+    weight: 700;
+    font-size: 20px;
+    margin: 0;
+    margin-left: 16px;
+  }
+`;
